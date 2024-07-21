7582dd34fa9628b6affa769f474085be3be2b827
Christian Sosa <Christiansosa@protonmail.com>
2024-07-20 23:10:05 -0400

    Tailwind Installed & Working
    tailwind.config.js created and added tailwind directive to styles.css

---
+++ angular-codebase/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  /**Add the paths to all of your template files in your tailwind.config.js file.*/
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
